43413c1557e5dbb709bf008077db0e49d2a62310
ironzhang <ironzhang@users.noreply.github.com>
2015-12-08 15:13:22 +0800

    注释lru包

--- lru/lru.go
+++ lru/lru.go
@@ -15,6 +15,7 @@ limitations under the License.
 */
 
 // Package lru implements an LRU cache.
+// lur缓存组件
 package lru
 
 import "container/list"
@@ -23,10 +24,12 @@ import "container/list"
 type Cache struct {
 	// MaxEntries is the maximum number of cache entries before
 	// an item is evicted. Zero means no limit.
+	// 最大缓存对象数
 	MaxEntries int
 
 	// OnEvicted optionally specificies a callback function to be
 	// executed when an entry is purged from the cache.
+	// 移除缓存对象时回调
 	OnEvicted func(key Key, value interface{})
 
 	ll    *list.List
@@ -36,6 +39,7 @@ type Cache struct {
 // A Key may be any value that is comparable. See http://golang.org/ref/spec#Comparison_operators
 type Key interface{}
 
+// 缓存对象，由键与值构成
 type entry struct {
 	key   Key
 	value interface{}
@@ -44,6 +48,8 @@ type entry struct {
 // New creates a new Cache.
 // If maxEntries is zero, the cache has no limit and it's assumed
 // that eviction is done by the caller.
+// 创建一个新的缓存
+// 如果maxEntries为0，则缓存对象数量没有上限
 func New(maxEntries int) *Cache {
 	return &Cache{
 		MaxEntries: maxEntries,
@@ -53,18 +59,23 @@ func New(maxEntries int) *Cache {
 }
 
 // Add adds a value to the cache.
+// 添加一个值到缓存
 func (c *Cache) Add(key Key, value interface{}) {
+	// 支持延迟初始化
 	if c.cache == nil {
 		c.cache = make(map[interface{}]*list.Element)
 		c.ll = list.New()
 	}
+	// 如果键在缓存中已存在，则将该缓存对象移到缓存队列头部，并更新其值
 	if ee, ok := c.cache[key]; ok {
 		c.ll.MoveToFront(ee)
 		ee.Value.(*entry).value = value
 		return
 	}
+	// 不存在，则构建一个缓存对象并压入缓存队列头部
 	ele := c.ll.PushFront(&entry{key, value})
-	c.cache[key] = ele
+	c.cache[key] = ele // 建立键与缓存对象的映射
+	// 如果缓存对象数超过上限，则将最长时间未使用的缓存对象移除
 	if c.MaxEntries != 0 && c.ll.Len() > c.MaxEntries {
 		c.RemoveOldest()
 	}
@@ -75,40 +86,43 @@ func (c *Cache) Get(key Key) (value interface{}, ok bool) {
 	if c.cache == nil {
 		return
 	}
+	// 根据键查找缓存对象
 	if ele, hit := c.cache[key]; hit {
-		c.ll.MoveToFront(ele)
+		c.ll.MoveToFront(ele) // 将缓存对象移到缓存队列头部
 		return ele.Value.(*entry).value, true
 	}
 	return
 }
 
 // Remove removes the provided key from the cache.
+// 移除指定的缓存对象
 func (c *Cache) Remove(key Key) {
 	if c.cache == nil {
 		return
 	}
 	if ele, hit := c.cache[key]; hit {
-		c.removeElement(ele)
+		c.removeElement(ele) // 移除缓存对象
 	}
 }
 
 // RemoveOldest removes the oldest item from the cache.
+// 移除最久未使用的缓存对象
 func (c *Cache) RemoveOldest() {
 	if c.cache == nil {
 		return
 	}
-	ele := c.ll.Back()
+	ele := c.ll.Back() // 取得缓存队列某位的缓存对象
 	if ele != nil {
-		c.removeElement(ele)
+		c.removeElement(ele) // 移除该对象
 	}
 }
 
 func (c *Cache) removeElement(e *list.Element) {
-	c.ll.Remove(e)
+	c.ll.Remove(e) // 从缓存列表中移除该元素
 	kv := e.Value.(*entry)
-	delete(c.cache, kv.key)
+	delete(c.cache, kv.key) // 从缓存映射表中移除该缓存对象
 	if c.OnEvicted != nil {
-		c.OnEvicted(kv.key, kv.value)
+		c.OnEvicted(kv.key, kv.value) // 移除时的回调处理
 	}
 }
 
