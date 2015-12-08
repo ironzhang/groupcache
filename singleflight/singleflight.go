ec8594cdd81c0a69c724641005174c9f85a888be
ironzhang <ironzhang@users.noreply.github.com>
2015-12-08 15:45:18 +0800

    注释singleflight包

--- singleflight/singleflight.go
+++ singleflight/singleflight.go
@@ -16,6 +16,7 @@ limitations under the License.
 
 // Package singleflight provides a duplicate function call suppression
 // mechanism.
+// singleflight包提供了一种抑制函数重复调用的机制
 package singleflight
 
 import "sync"
@@ -38,27 +39,41 @@ type Group struct {
 // sure that only one execution is in-flight for a given key at a
 // time. If a duplicate comes in, the duplicate caller waits for the
 // original to complete and receives the same results.
+// 该函数的作用是将重复的(key相同)多次函数调用归并为一次函数调用
+// 后调的函数会等待前调的函数执行完毕，并使用其结果来作为返回值
+// eg.
+// 在两个goroutine中并发执行如下代码
+// goroutine1
+//   val, err := g.Do("iron", LoadUserFromDB)
+// goroutine2
+//   val, err := g.Do("iron", LoadUserFromDB)
+// 只有一个goroutine会真正调用LoadUserFromDB，另一个会等待真正调用的结果，这也是这个包叫singleflight的原因吧 ^_^
 func (g *Group) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
 	g.mu.Lock()
 	if g.m == nil {
 		g.m = make(map[string]*call)
 	}
+	// 如果函数已在调用中，则无需执行，等待之前调用的函数执行完成即可
 	if c, ok := g.m[key]; ok {
 		g.mu.Unlock()
-		c.wg.Wait()
-		return c.val, c.err
+		c.wg.Wait() // 等待函数执行完毕
+		return c.val, c.err // 返回函数的执行结果
 	}
+	// 没有在调用中，则新建一个call
 	c := new(call)
 	c.wg.Add(1)
 	g.m[key] = c
 	g.mu.Unlock()
 
+	// 执行函数，并将结果保存在call中
 	c.val, c.err = fn()
-	c.wg.Done()
+	c.wg.Done() // 函数执行完毕，解锁其他在等待中的执行流
 
+	// 函数执行完毕，移除call
 	g.mu.Lock()
 	delete(g.m, key)
 	g.mu.Unlock()
 
+	// 返回执行结果
 	return c.val, c.err
 }
